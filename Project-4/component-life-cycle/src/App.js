456f23595519c1711b703163e630e6c441040325
sergio rodriguez <sergrodri92@gmail.com>
2017-06-04 10:01:33 -0500

    finished simple random number generator

--- Project-4/component-life-cycle/src/App.js
+++ Project-4/component-life-cycle/src/App.js
@@ -10,9 +10,56 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <Body />
+      </div>
+    );
+  }
+}
+
+class Body extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      r: 0
+    };
+    this.getRandomNumber = this.getRandomNumber.bind(this);
+  }
+
+  getRandomNumber(){
+    console.log("random number called");
+    this.setState({r: Math.floor(Math.random() * 10)})
+  }
+
+  render(){
+    return(
+      <div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.getRandomNumber}>Random Number</button>
+        <Numbers  myNumber={this.state.r}/>
+      </div>
+    );
+  }
+}
+
+class Numbers extends Component {
+  componentWillMount(){
+    console.log("componet will mount");
+  }
+  componentDidMount(){
+    console.log("componentDidMount");
+  }
+  componentWillReceiveProps(newProps){
+    console.log("componentWillReceiveProps");
+  }
+
+  render(){
+    return(
+      <div>
+        <br   />
+        {this.props.myNumber}
       </div>
     );
   }
