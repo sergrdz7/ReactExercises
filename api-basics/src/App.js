0d5345ba93f5affd481bc5ffd29b7c4c3fa0eb8f
sergio rodriguez <sergrodri92@gmail.com>
2017-06-27 20:03:20 -0500

    api-basics

---
+++ api-basics/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Apicall from './Apicall.js';
+
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <Apicall />
+
+      </div>
+    );
+  }
+}
+
+export default App;
